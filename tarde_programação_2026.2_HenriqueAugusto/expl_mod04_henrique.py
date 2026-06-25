0da8d5a430a6234c9661cb63119998ce80b752e5
Aluno Vocacao Henrique Augusto <henrikaique121@gmail.com>
2026-06-25 16:57:05 -0300

    Exemplo de adicção em lista

---
+++ tarde_programação_2026.2_HenriqueAugusto/expl_mod04_henrique.py
@@ -0,0 +1,13 @@
+ frutas = ["Uva", "banana", "maçã", "acerola", "limão"] 
+print(frutas)
+
+print('Qual a fruta que você deseja adicionar na lista: ?')
+
+nova_fruta = input('A fruta escolhida foi: ')
+frutas.append(nova_fruta)
+print(frutas)
+
+
+
+
+
